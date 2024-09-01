dffcadbf5ea43f62a11edbc78660e3d156fa8729
Mohd Suhail <93874933+TheMohdSuhail@users.noreply.github.com>
2024-09-02 02:43:38 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 
+app.use(cors({
+  origin: 'https://msp-portfolio.onrender.com', // Replace with your frontend domain
+  credentials: true,
+}));
 const portfolioRoute = require("./routes/portfolioRoute");
 
 app.use(express.json());
