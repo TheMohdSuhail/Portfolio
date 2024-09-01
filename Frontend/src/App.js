726008a91a50ee09f801c9880be3e763f035c5cc
Mohd Suhail <93874933+TheMohdSuhail@users.noreply.github.com>
2024-09-02 01:16:01 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,58 +1,67 @@
-// src/App.js
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import { useEffect, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Loader from './components/Loader';
 import { useDispatch, useSelector } from 'react-redux';
-import { SetPortfolioData, ShowLoading, HideLoading, ReloadData } from './redux/rootSlice';
+import { SetPortfolioData,ShowLoading, HideLoading, ReloadData } from './redux/rootSlice';
 import Admin from './pages/Admin';
 import Login from './pages/Admin/Login';
 
 function App() {
-  const { loading, portfolioData, reloadData } = useSelector(state => state.root);
+  // const [showLoading, setShowLoading] = useState(false);
+  const {loading, portfolioData,reloadData} = useSelector(state => state.root)
   const dispatch = useDispatch();
 
-  // Set base URL for Axios
-  const api = axios.create({
-    baseURL: 'https://portfolio-0fue.onrender.com/api/portfolio',
-  });
-
-  const getPortfolioData = useCallback(async () => {
+  const getPortfolioData = async () => {
     try {
       dispatch(ShowLoading());
-      const response = await api.get('/get-portfolio-data');
+      const response = await axios.get('https://msp-portfolio.onrender.com/api/portfolio/get-portfolio-data');
       dispatch(SetPortfolioData(response.data));
       dispatch(ReloadData(false));
     } catch (error) {
-      console.error('Error fetching portfolio data:', error);
-    } finally {
+      // dispatch(HideLoading());
+      console.log(error);
+    } 
+    finally {
       dispatch(HideLoading());
     }
-  }, [dispatch, api]);
+  };
+  
 
   useEffect(() => {
     if (!portfolioData) {
-      getPortfolioData();
+      getPortfolioData()
     }
-  }, [portfolioData, getPortfolioData]);
+  }, [portfolioData]);
 
-  useEffect(() => {
-    if (reloadData) {
+  useEffect(()=>{
+    if(reloadData){
       getPortfolioData();
     }
-  }, [reloadData, getPortfolioData]);
+  },[reloadData])
+
+
+//  useEffect(() =>{
+//     console.log(portfolioData);
+    
+//  },[portfolioData])
 
   return (
+
     <BrowserRouter>
-      {loading && <Loader />}
+      {loading ? <Loader /> : null}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/admin' element={<Admin />} />
         <Route path='/admin-login' element={<Login />} />
       </Routes>
+
     </BrowserRouter>
+
+    // <div className="App flex h-screen items-center justify-center">
+    //   <h1 className='text-5xl'>Hello World!</h1>
+    // </div>
   );
 }
 
