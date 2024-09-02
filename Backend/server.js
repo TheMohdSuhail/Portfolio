d1da4058f0e94ee9048b22dfe651cd7b8c178384
Mohd Suhail <93874933+TheMohdSuhail@users.noreply.github.com>
2024-09-02 17:15:21 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,11 +21,11 @@ app.use("/api/portfolio", portfolioRoute);
 // Serve static files in production
 if (process.env.NODE_ENV === "production") {
     // Serve static files from the React app
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "Frontend/build")));
 
     // Handle React routing, return all requests to React app
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+        res.sendFile(path.join(__dirname, "Frontend/build", "index.html"));
     });
 }
 
