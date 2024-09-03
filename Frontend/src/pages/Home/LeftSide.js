0d1581da0af94c9b23c43e4fc1491c9a7141cc7b
Mohd Suhail <93874933+TheMohdSuhail@users.noreply.github.com>
2024-09-04 02:43:28 +0530

    Update LeftSide.js

--- Frontend/src/pages/Home/LeftSide.js
+++ Frontend/src/pages/Home/LeftSide.js
@@ -5,25 +5,23 @@ const LeftSide = () => {
         <div className='fixed left-0 bottom-0 px-10 sm:static'>
             <div className='flex flex-col items-center'>
                 <div className='flex flex-col gap-3 sm:flex-row sm:pb-3'>
-                    <a href="https://www.facebook.com">
+                    <a href="https://www.facebook.com/themsp1" target="_blank" rel="noopener noreferrer">
                         <i className="ri-facebook-circle-line text-gray-400"></i>
                     </a>
-                    <a href="mailto:your_email@example.com">
+                    <a href="mailto:mohdsuhail.istiyak@gmail.com">
                         <i className="ri-mail-line text-gray-400"></i>
                     </a>
-                    <a href="https://www.instagram.com">
+                    <a href="https://www.instagram.com/themsp3" target="_blank" rel="noopener noreferrer">
                         <i className="ri-instagram-line text-gray-400"></i>
                     </a>
-                    <a href="https://www.linkedin.com">
+                    <a href="https://www.linkedin.com/in/themohdsuhail" target="_blank" rel="noopener noreferrer">
                         <i className="ri-linkedin-box-line text-gray-400"></i>
                     </a>
-                    <a href="https://github.com">
+                    <a href="https://github.com/TheMohdSuhail" target="_blank" rel="noopener noreferrer">
                         <i className="ri-github-line text-gray-400"></i>
                     </a>
                 </div>
-                <div className='w-[1px] h-32 bg-[#125f63] sm:hidden'>
-
-                </div>
+                <div className='w-[1px] h-32 bg-[#125f63] sm:hidden'></div>
             </div>
         </div>
     )
